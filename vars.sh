d2d0a6e5811f63ac802595d2a358912ba7080f2c
Mikael Vesavuori <mikaelvesavuori@gmail.com>
2020-10-19 09:16:52 +0200

    ci: update ci config

--- vars.sh
+++ vars.sh
@@ -3,4 +3,4 @@ export REPO_NAME="gql-api"
 export RUN_SERVICE_NAME="cloud-run-ts-apollo-graphql"
 export LOCATION="europe-north1"
 export DESCRIPTION="Apollo Server (GraphQL) API using Typescript, deployed on Google Cloud Run"
-export IMAGE='gql-api'
+export IMAGE="gql-api"
